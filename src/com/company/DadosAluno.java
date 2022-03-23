c60483d3f4688eef8affb2072091bdf57f192da6
RobertoBarbosa25 <barbosaddt337@hotmail.com>
2022-03-23 03:29:33 -0300

    Faça um programa que leia as seguintes informações: Nome completo; Nome do curso; Número do período (Exemplo: 1,2,3,4…) Valor da primeira Nota; Valor da segunda Nota; Quantidade de faltas. Esse problema deve imprimir todas as informações solicitadas; Esse problema deve imprimir a média entre as notas;

---
+++ src/com/company/DadosAluno.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.util.Scanner;
+
+public class DadosAluno {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nome completo: " );
+        String nome = scanner.nextLine();
+        System.out.println("O que você cursa: ");
+        String curso = scanner.nextLine();
+        System.out.println("Qual periodo: ");
+        int periodo = scanner.nextInt();
+        System.out.println("Primeira nota: ");
+        float primeiraNota = scanner.nextFloat();
+        System.out.println("Segunda nota: ");
+        float segundaNota = scanner.nextFloat();
+        System.out.println("Quantidade de faltas: ");
+        int faltas = scanner.nextInt();
+
+
+
+        System.out.println("Nome: "+nome);
+        System.out.println("Curso: "+curso);
+        System.out.println("Periodo: "+periodo);
+        System.out.println("Quantidade de faltas: "+faltas);
+        float media = (primeiraNota + segundaNota) / 2;
+        System.out.println("Média do aluno: "+media);
+
+
+        }
+}
